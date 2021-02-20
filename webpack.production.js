ffb391e63d98b161a0934114289ec3b36f38dfb1
Luca Zampetti <lzampetti@gmail.com>
2021-02-20 23:46:32 +0100

    tttoe-reducer

--- webpack.production.js
+++ webpack.production.js
@@ -12,15 +12,23 @@ const config = {
     publicPath: '/',
     filename: '[name].[contenthash].js',
   },
+  html: {
+    template: './src/index.html',
+    filename: 'index.html',
+    title: 'TTToe',
+    description: 'TTToe',
+    image: 'https://avatars.githubusercontent.com/u/1841442?s=460&u=9d963e264cc39206782c9e0dbfdbdb0c6d25b477&v=4',
+    url: 'https://actarian.github.io/tttoe/tttoe-reducer',
+  },
   performance: {
-    hints: 'warning', // enum
+    hints: 'warning',
     maxAssetSize: 200000, // int (in bytes),
     maxEntrypointSize: 400000, // int (in bytes)
     assetFilter: function(assetFilename) {
-      // Function predicate that provides asset filenames
       return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
     }
   },
+  analyzer: true,
 };
 
 module.exports = config;
