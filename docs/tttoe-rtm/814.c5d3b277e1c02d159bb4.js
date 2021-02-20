a3a515dab45d80606e6ad7b2699b41111d45b5f6
Luca Zampetti <lzampetti@gmail.com>
2021-02-20 23:31:25 +0100

    tttoe-rtm

---
+++ docs/tttoe-rtm/814.c5d3b277e1c02d159bb4.js
@@ -0,0 +1,3 @@
+/*! For license information please see 814.c5d3b277e1c02d159bb4.js.LICENSE.txt */
+(self.webpackChunktttoe=self.webpackChunktttoe||[]).push([[814],{814:(e,t,r)=>{"use strict";function a(){return new Worker(r.p+"game.worker.8a09d0a9028b16cacc38.worker.js")}r.r(t),r.d(t,{default:()=>a})}}]);
+//# sourceMappingURL=814.35f6ee2980fe58233e8d65b495663da6..js.map
--- webpack.development.js
+++ webpack.development.js
@@ -9,8 +9,24 @@ const config = {
   mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '',
+    publicPath: '/tttoe/tttoe-rtm/',
     filename: '[name].[contenthash].js',
+  },
+  html: {
+    template: './src/index.html',
+    filename: 'index.html',
+    title: 'TTToe',
+    description: 'TTToe',
+    image: 'https://avatars.githubusercontent.com/u/1841442?s=460&u=9d963e264cc39206782c9e0dbfdbdb0c6d25b477&v=4',
+    url: 'https://actarian.github.io/tttoe/tttoe-rtm',
+  },
+  devServer: {
+    contentBase: [path.join(__dirname, 'assets')],
+    contentBasePublicPath: ['/tttoe/tttoe-rtm/'],
+    host: '0.0.0.0',
+    openPage: 'tttoe/tttoe-rtm',
+    port: 9000,
+    publicPath: '/tttoe/tttoe-rtm/',
   },
 };
 
