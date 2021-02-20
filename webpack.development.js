b43d77a1e6c44046ab7120069e9b5cce14d5fa48
Luca Zampetti <lzampetti@gmail.com>
2021-02-20 23:53:07 +0100

    tttoe-state

--- webpack.development.js
+++ webpack.development.js
@@ -9,8 +9,24 @@ const config = {
   mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '',
+    publicPath: '/tttoe/tttoe-state/',
     filename: '[name].[contenthash].js',
+  },
+  html: {
+    template: './src/index.html',
+    filename: 'index.html',
+    title: 'TTToe',
+    description: 'TTToe',
+    image: 'https://avatars.githubusercontent.com/u/1841442?s=460&u=9d963e264cc39206782c9e0dbfdbdb0c6d25b477&v=4',
+    url: 'https://actarian.github.io/tttoe/tttoe-state',
+  },
+  devServer: {
+    contentBase: [path.join(__dirname, 'assets')],
+    contentBasePublicPath: ['/tttoe/tttoe-state/'],
+    host: '0.0.0.0',
+    openPage: 'tttoe/tttoe-state',
+    port: 9000,
+    publicPath: '/tttoe/tttoe-state/',
   },
 };
 
